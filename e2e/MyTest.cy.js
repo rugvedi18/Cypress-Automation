dca331856ee537bb476b3e4d06a4582cf47a0b5a
rugvedi <rugvedij1804@gmail.com>
2023-10-12 09:06:48 +0530

    initial commit

---
+++ e2e/MyTest.cy.js
@@ -0,0 +1,13 @@
+describe('My Test', () => {
+    it('test1', () => {
+        // launch an application - positive test
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq','OrangeHRM')
+    })
+
+    it('test2', () => {
+        // launch an application - negative test
+        cy.visit("https://opensource-demo.orangehrmlive.com/")
+        cy.title().should('eq','OrangeHRM123')
+    })
+})
