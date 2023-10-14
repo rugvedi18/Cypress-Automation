d3b3f8624670912601b0577f116c038a37ecf0c9
rugvedi <rugvedij1804@gmail.com>
2023-10-14 22:45:56 +0530

    - ecommerce app

--- e2e/CssLocatorsDrive.cy.js
+++ e2e/CssLocatorsDrive.cy.js
@@ -4,21 +4,25 @@ describe('CssLocators', () => {
 
         cy.visit("https://dapper-pastelito-04ff24.netlify.app/") // website
 
-        cy.get(".sc-crozmw").click() // by class (shop now)
+        cy.get(".sc-crozmw").click() // shop now
 
-        cy.get(".bKVxEB").type("shoes")
+        cy.get(".bKVxEB").type("shoes") // search for shoes
 
-        cy.get(".search").click()
+        cy.get(".search").click() // click on search
 
-        cy.get('[alt="mens-shoes60"]').click()
+        cy.get('[alt="mens-shoes60"]').click() // open first shoes
 
-        //cy.scrollTo(0, 300);
+        //cy.scrollTo(0, 300); // in px
 
-        //cy.get('button.ui.large.button:contains("Add to cart")').click({ multiple: true }); // will not work beacuse Cypress doesn't support multiple browsers windows/tabs.
+        cy.get('.tablet.only.computer.only.two.column.row .button.ui.large.button:contains("Add to cart")').click(); // parent class and child class
 
-        cy.scrollTo('top');
+        cy.scrollTo('top')
 
-        cy.get('[href="/shop"]').click()
+        cy.get('.sc-ispOId.jYrqzs').click() // click on cart to see list
+
+        // cy.get('[href="/shop"]').click()
+
+        cy.get(".computer.only.four.wide.column.right-col--sticky .ui.button").click()
 
     })
 })
