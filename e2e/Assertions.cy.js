3163316792ae3546054e89e2167915c64babebbf
rugvedi <rugvedij1804@gmail.com>
2023-10-19 18:32:34 +0530

    - implicit and explicit assertions

--- e2e/Assertions.cy.js
+++ e2e/Assertions.cy.js
@@ -1,5 +1,22 @@
 // website - orangeHRM demo
 
+/*
+
+Cypress Assertions (search on google) - derived from chai library
+------------------------------------------------
+
+1. Implicit Assertions - should, and
+
+eq      - not.eq
+contain - not.contain
+include - not.include
+exist   - not.exist
+
+2. Explicit Assertions - expect (used in BDD approach framework), assert (used in TDD approach framework)
+
+*/
+
+
 describe("Assertions demo", () => {
 
     // should - has two parameters .should('key/keyword','value')
@@ -21,15 +38,44 @@ describe("Assertions demo", () => {
         .should('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') */
 
         // Approach 3
-       /* cy.url().should('include', 'orangehrmlive.com')
-        .and('contain','orangehrm')
-        .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') */
+        /* cy.url().should('include', 'orangehrmlive.com')
+         .and('contain','orangehrm')
+         .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login') */
 
-        // not contain
         cy.url().should('include', 'orangehrmlive.com')
-        .and('contain','orangehrm')
-        .and('not.contain','greenhrm')
-        .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+            .and('contain', 'opensource-demo')
+            .and('not.contain', 'greenhrm') // not contain
+            .and('eq', 'https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
 
+        cy.title().should('include', 'OrangeHRM') // title of the page
+
+        cy.get('.orangehrm-login-branding > img').should('be.visible').and('exist') // logo
+
+        cy.get("input[placeholder='Username']").type('Admin') // Username as Admin
+        cy.get("input[placeholder='Username']").should('have.value', 'Admin') // to check value
+        // .and('have.value', 'Admin')
     })
+
+    // Explicit Assertions
+    it("Explicit assertions", () => {
+
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+        cy.get("input[placeholder='Username']").type('Admin')
+        cy.get("input[placeholder='Password']").type('admin123')
+        cy.get("button[type='submit']").click()
+
+        let expName = "as s";
+
+        cy.get(".oxd-userdropdown-name").then((x) => {
+
+            // BDD style
+            let actName = x.text()
+            expect(actName).to.equal(expName)
+
+            // TDD style
+            assert.equal(actName, expName)
+            assert.notEqual(actName, "expName")
+        })
+    })
+
 })
