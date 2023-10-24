c1e650de28970ab8a8a024a1ed3ec4e20ec1d427
rugvedi <rugvedij1804@gmail.com>
2023-10-24 23:31:14 +0530

    - dropdown file created

--- e2e/DropDown.cy.js
+++ e2e/DropDown.cy.js
@@ -3,11 +3,10 @@
 // Types of dropdowns - select
 
 describe("Drop Down", () => {
+  it("Dropdown with select", () => {
+    cy.visit("https://testautomationpractice.blogspot.com/");
 
-    it("Dropdown with select", () => {
-        cy.visit("https://testautomationpractice.blogspot.com/")
-
-        // select from drop-down
-        cy.get("#country").select('Germany').should('have.value', 'germany')
-    })
-})
+    // select from drop-down
+    cy.get("#country").select("Germany").should("have.value", "germany");
+  });
+});
