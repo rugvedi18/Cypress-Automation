3e8b6f6222e34af64ea5e29d3b9088531e287100
rugvedi <rugvedij1804@gmail.com>
2023-11-20 22:10:07 +0530

    - mouse operations completed

--- e2e/MouseOperations.cy.js
+++ e2e/MouseOperations.cy.js
@@ -1,7 +1,8 @@
-// demo.opencart.com
+import "cypress-iframe"
+require("@4tw/cypress-drag-drop")
 
 describe("Mouse Operations", () => {
-  it.only("Mouse Hover", () => {
+  it("Mouse Hover", () => {
     cy.visit("https://demo.opencart.com/")
 
     cy.get(
@@ -19,11 +20,67 @@ describe("Mouse Operations", () => {
       .and("have.text", "Mac (1)")
   })
 
-  it("Right Click", () => {})
+  it("Right Click", () => {
+    cy.visit("https://swisnl.github.io/jQuery-contextMenu/demo.html")
 
-  it("Double Click", () => {})
+    // Approach 1
+    // cy.get(".context-menu-one.btn.btn-neutral").trigger("contextmenu")
+    // cy.get(".context-menu-icon-copy > span").should("be.visible", "Copy")
 
-  it("Drag and Drop using Plugin", () => {})
+    // Approach 2
+    cy.get(".context-menu-one.btn.btn-neutral").rightclick()
+    cy.get(".context-menu-icon-copy > span").should("be.visible", "Copy")
+  })
 
-  it("Scrolling Page", () => {})
+  it("Double Click", () => {
+    cy.visit(
+      "https://www.w3schools.com/tags/tryit.asp?filename=tryhtml5_ev_ondblclick3"
+    )
+    cy.frameLoaded("#iframeResult") // to load the iframe
+
+    // Approach 1
+    // cy.iframe("#iframeResult").find("button[ondblclick='myFunction()']").trigger("dblclick")
+    // cy.iframe("#iframeResult").find("#field2").should("have.value", "Hello World!")
+
+    // Approach 2
+    cy.iframe("#iframeResult")
+      .find("button[ondblclick='myFunction()']")
+      .dblclick()
+    cy.iframe("#iframeResult")
+      .find("#field2")
+      .should("have.value", "Hello World!")
+  })
+
+  it("Drag and Drop using Plugin", () => {
+    // npm i @4tw/cypress-drag-drop - plugin to drag and drop action
+    cy.visit(
+      "https://www.javascripttutorial.net/sample/webapis/drag-n-drop-basics/"
+    )
+    cy.get(".item").drag(
+      "body > div:nth-child(1) > div:nth-child(2) > div:nth-child(2)"
+    )
+    // ,{force:true} - if it shows item not found error
+  })
+
+  it.only("Scrolling Page", () => {
+    cy.visit(
+      "https://www.countries-ofthe-world.com/flags-of-the-world.html"
+    ).contains("Country flags of the world")
+    // scroll the page till the element get displayed, slowly scrolling using duration
+    cy.get("img[alt='Flag of India']")
+      .scrollIntoView({ duration: 3000 })
+      .should("be.visible", "India")
+
+    cy.wait(1000)
+
+    cy.get("img[alt='Flag of Australia']")
+      .scrollIntoView({ duration: 3000 })
+      .should("be.visible", "Australia")
+
+    cy.wait(1000)
+
+    cy.get(".read-next")
+      .scrollIntoView({ duration: 3000 })
+      .should("be.visible", "READ NEXT")
+  })
 })
