7113639e750078c1d8d4091cc4dcbde303c84076
rugvedi <rugvedij1804@gmail.com>
2024-01-03 16:48:42 +0530

    - cypress api testing POST methos

--- e2e/APITesting/HttpRequests.cy.js
+++ e2e/APITesting/HttpRequests.cy.js
@@ -1,7 +1,23 @@
+// cy.request(request type, url, body)
+
 describe("HTTP Requests", () => {
   it("GET Call", () => {
     cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1")
       .its("status")
       .should("equal", 200)
+  })
+
+  it("POST Call", () => {
+    cy.request({
+      method: "POST",
+      url: "https://jsonplaceholder.typicode.com/posts/",
+      body: {
+        title: "Test post",
+        body: "This is post call",
+        userId: 1,
+      },
+    })
+      .its("status")
+      .should("equal", 201)
   })
 })
