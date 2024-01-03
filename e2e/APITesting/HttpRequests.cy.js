59eecf2edd681ded0995d8609072776529d5de0b
rugvedi <rugvedij1804@gmail.com>
2024-01-03 13:52:11 +0530

    - cypress api testing get method

--- e2e/APITesting/HttpRequests.cy.js
+++ e2e/APITesting/HttpRequests.cy.js
@@ -1,3 +1,7 @@
 describe("HTTP Requests", () => {
-  it("GET Call", () => {})
+  it("GET Call", () => {
+    cy.request("GET", "https://jsonplaceholder.typicode.com/posts/1")
+      .its("status")
+      .should("equal", 200)
+  })
 })
