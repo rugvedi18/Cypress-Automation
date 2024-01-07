5e708faf422db2720cfd1d76eced5d7b20ec97a3
rugvedi <rugvedij1804@gmail.com>
2024-01-07 15:45:03 +0530

    - post call

--- e2e/APITesting/PostCalls.cy.js
+++ e2e/APITesting/PostCalls.cy.js
@@ -2,7 +2,7 @@ describe("Different methods of Post call", () => {
   it("Approach 1 - Hard coded json object", () => {
     const requestBody = {
       tourist_name: "Mike",
-      tourist_email: "abc456@gmail.com",
+      tourist_email: "abc23456@gmail.com",
       tourist_location: "Paris",
     }
     cy.request({
@@ -10,7 +10,13 @@ describe("Different methods of Post call", () => {
       url: "http://restapi.adequateshop.com/api/Tourist",
       body: requestBody,
     })
-      .its("status")
-      .should("eq", 201)
+      // .its("status")
+      // .should("eq", 201)
+      .then((response) => {
+        expect(response.status).to.eq(201)
+        expect(response.body.tourist_name).to.eq("Mike")
+        expect(response.body.tourist_email).to.eq("abc23456@gmail.com")
+        expect(response.body.tourist_location).to.eq("Paris")
+      })
   })
 })
